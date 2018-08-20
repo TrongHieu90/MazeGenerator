0b4df69ac04ea7627d4d8ea72e8951e2e0f19a1d
Trong Hieu Nguyen <tronghieu.designer@gmail.com>
2018-08-20 20:32:26 +0200

    Add files via upload

---
+++ MazeGeneration/MazeGeneration/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace MazeGeneration
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Canvas = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Canvas
+            // 
+            this.Canvas.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Canvas.Location = new System.Drawing.Point(12, 49);
+            this.Canvas.Name = "Canvas";
+            this.Canvas.Size = new System.Drawing.Size(400, 400);
+            this.Canvas.TabIndex = 0;
+            this.Canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(390, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Maze Generator using Depth-First Search and Recursive BackTracking Algorithm";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 472);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(270, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter integer number to generate Maze (recommend 10)";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(309, 469);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(103, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 495);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 43);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Generate Maze";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 550);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Canvas);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Maze Generator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Canvas;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ MazeGeneration/MazeGeneration/Form1.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MazeGeneration
+{
+    public partial class Form1 : Form
+    {
+        protected int c_width;
+        protected static int cols, rows;
+        protected Spot[,] grid;
+        protected Stack<Spot> stack = new Stack<Spot>();
+
+        protected Spot currentSpot;
+        protected Spot next;
+
+        Graphics g;
+        Pen blackPen;
+        Pen whitePen;
+        SolidBrush blackBrush;
+
+        protected int inputInt; //input to set cols and rows
+        //protected bool canDrawGrid; 
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            g = Canvas.CreateGraphics();
+            blackPen = new Pen(Color.Black, 2);
+            whitePen = new Pen(Color.White, 5);
+            blackBrush = new SolidBrush(Color.Black);
+        }
+
+        public class Spot : Form1
+        {
+            public int i;
+            public int j;
+            public bool[] walls = { true, true, true, true };
+            public List<Spot> neighbors;
+            public bool visited = false;
+
+            public Spot(int i, int j)
+            {
+                this.i = i;
+                this.j = j;
+            }
+            public Spot CheckNeighbors(Spot[,] grid)
+            {
+                int i = this.i;
+                int j = this.j;
+
+                neighbors = new List<Spot>();
+
+                Spot right, up, down, left;
+                if (i < cols - 1)
+                {
+                    //right adjacent spot
+                    right = grid[i + 1, j];
+                    if (right != null && !right.visited)
+                    {
+                        neighbors.Add(right);
+                    }
+                }
+                if (i > 0)
+                {
+                    left = grid[i - 1, j];
+                    //left adjacent spot
+                    if (left != null && !left.visited)
+                    {
+                        neighbors.Add(left);
+                    }
+                }
+                if (j < rows - 1)
+                {
+                    down = grid[i, j + 1];
+
+                    //down adjacent spot
+                    if (down != null && !down.visited)
+                    {
+                        neighbors.Add(down);
+                    }
+                }
+                if (j > 0)
+                {
+                    up = grid[i, j - 1];
+                    //up adjacent spot
+                    if (up != null && !up.visited)
+                    {
+                        neighbors.Add(up);
+                    }
+                }
+                
+                if(neighbors.Count > 0)
+                {
+                    RNGCryptoServiceProvider Rand = new RNGCryptoServiceProvider();
+                    var r = RandomInteger(Rand, 0, neighbors.Count);
+                    return neighbors[r];
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private int RandomInteger(RNGCryptoServiceProvider Rand, int min, int max)
+        {
+            uint scale = uint.MaxValue;
+            while (scale == uint.MaxValue)
+            {
+                // Get four random bytes.
+                byte[] four_bytes = new byte[4];
+                Rand.GetBytes(four_bytes);
+
+                // Convert that into an uint.
+                scale = BitConverter.ToUInt32(four_bytes, 0);
+            }
+
+            // Add min to the scaled difference between max and min.
+            return (int)(min + (max - min) *
+                (scale / (double)uint.MaxValue));
+        }
+
+        private void RemoveWalls(Spot a, Spot b)
+        {
+            var x = a.i - b.i;
+            if (x == 1)
+            {
+                a.walls[3] = false;
+                b.walls[1] = false;
+            }
+
+            if (x == -1)
+            {
+                a.walls[1] = false;
+                b.walls[3] = false;
+            }
+            var y = a.j - b.j;
+            if (y == 1)
+            {
+                a.walls[0] = false;
+                b.walls[2] = false;
+            }
+
+            if (y == -1)
+            {
+                a.walls[2] = false;
+                b.walls[0] = false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox1.Text, out inputInt))
+            {
+                if (inputInt == 0)
+                {
+                    string message = "Cannot use 0 as integer. Please Enter another Integer number.";
+                    string caption = "Error Detected in Input";
+
+                    MessageBoxButtons buttons = MessageBoxButtons.OK;
+                    DialogResult result;
+
+                    result = MessageBox.Show(message, caption, buttons);
+
+                    //if (result == DialogResult.OK)
+                    //{
+
+                    //}
+                }
+                else
+                {
+                    cols = inputInt;
+                    rows = inputInt;
+
+                    c_width = Canvas.Width / cols;
+
+                    grid = new Spot[cols, rows];
+
+                    //building grid
+                    for (int i = 0; i < cols; i++)
+                    {
+                        for (int j = 0; j < rows; j++)
+                        {
+                            grid[i, j] = new Spot(i, j);
+                        }
+                    }
+
+                    currentSpot = grid[0, 0];
+
+                    while (true)
+                    {
+                        currentSpot.visited = true;
+
+                        //Using recursive backtracker algorithm
+                        //https://en.wikipedia.org/wiki/Maze_generation_algorithm#Recursive_backtracker
+
+                        //step 1
+                        next = currentSpot.CheckNeighbors(grid);
+
+                        if (next != null)
+                        {
+                            next.visited = true;
+
+                            //step 2
+                            stack.Push(currentSpot);
+                            //Console.WriteLine($"stack count is {stack.Count}");
+
+                            //step 3
+                            RemoveWalls(currentSpot, next);
+
+                            //step 4
+                            currentSpot = next;
+                            //Console.WriteLine($"currentSpot is {currentSpot.i} and {currentSpot.j}");
+                        }
+                        else if (stack.Count > 0)
+                        {
+                            currentSpot = stack.Pop();
+                            //Console.WriteLine($"pop a stack and count is {stack.Count}");
+                            //Console.WriteLine($"currentSpot after pop is {currentSpot.i} and {currentSpot.j}");
+
+                        }
+                        else if (next == null)
+                        {
+                            break;
+                        }
+
+                        //Call DrawGrid here (inside while loop) when we want to see how it works step by step. Really slow down the program
+                        //because we used 2d array and how each element is accessed in 2d array(?) and how WF implements draw graphics.
+                        //DrawGrid();
+                    }
+
+                    //Call DrawGrid outside while loop when the maze has already been generated. Really fast visualization
+                    DrawGrid();
+                }             
+            }
+        }
+
+        private void DrawGrid()
+        {
+            for (int i = 0; i < cols; i++)
+            {
+                for (int j = 0; j < rows; j++)
+                {
+                    int x = i * c_width;
+                    int y = j * c_width;
+
+                    if (grid[i, j].walls[0])
+                    {
+                        g.DrawLine(whitePen, x, y, x + c_width, y);
+                    }
+                    if (grid[i, j].walls[1])
+                    {
+
+                        g.DrawLine(whitePen, x + c_width, y, x + c_width, y + c_width);
+                    }
+                    if (grid[i, j].walls[2])
+                    {
+
+                        g.DrawLine(whitePen, x + c_width, y + c_width, x, y + c_width);
+                    }
+                    if (grid[i, j].walls[3])
+                    {
+
+                        g.DrawLine(whitePen, x, y + c_width, x, y);
+                    }
+
+                    if (grid[i, j].visited)
+                    {
+                        Rectangle Rect = new Rectangle(x, y, c_width, c_width);
+
+                        g.FillRectangle(blackBrush, Rect);
+                    }
+                }
+            }
+        }
+    }
+}
